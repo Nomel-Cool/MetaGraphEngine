6cbe27e7dfc8b44db12dc14da460ab54faf5e73d
Nomel-Cool <619471786@qq.com>
2024-12-03 00:45:16 +0800

    添加Redis动态库头文件

---
+++ headers/RedisClient.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef REDIS_CLIENT
+#define REDIS_CLIENT
+
+#include <iostream>
+#include <string>
+#include <functional>
+#include <hiredis.h>
+
+class RedisClient {
+public:
+    RedisClient(const std::string& host, int port);
+
+    ~RedisClient();
+
+    void SelectDB(int dbIndex);
+
+    std::string Get(const std::string& key);
+
+    template<typename T>
+    void Set(const std::string& key, const T& value, std::function<std::string(const T&)> toStringFunc) {
+        std::string valueStr = toStringFunc(value); // 使用传入的转换函数
+        redisReply* reply = static_cast<redisReply*>(redisCommand(context, "SET %s %s", key.c_str(), valueStr.c_str()));
+        if (reply == nullptr) {
+            std::cerr << "SET command failed" << std::endl;
+        }
+        freeReplyObject(reply);
+    }
+
+    void Set(const std::string& key, const std::string& value);
+
+    void Del(const std::string& key);
+
+    void FlushDB();
+
+private:
+    redisContext* context;
+};
+
+
+#endif // !REDIS_CLIENT
