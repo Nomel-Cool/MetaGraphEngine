0e9c42b314c153ac599579ddc9ebf187d3b7e10b
Nomel-Cool <619471786@qq.com>
2024-12-06 19:32:01 +0800

    1.添加兼容圆的椭圆算法；2.修复算法在曲线在y方向过于陡峭时无像素的bug

--- cpps/RenderKernel.cpp
+++ cpps/RenderKernel.cpp
@@ -1,5 +1,11 @@
 #include "RenderKernel.h"
 
+RenderCU::RenderCU()
+{
+    render_functions["BresenhamLine"] = std::bind(&RenderCU::BresenhamLine, this, std::placeholders::_1);
+    render_functions["BresenhamEllipse"] = std::bind(&RenderCU::BresenhamEllipse, this, std::placeholders::_1);
+}
+
 QString GraphFactory::Request4Model(const QString& model_name)
 {
     redis_client.SelectDB(0);
@@ -8,7 +14,7 @@ QString GraphFactory::Request4Model(const QString& model_name)
     {
          GraphModel graph_model;
          //std::string automata_xml_path = QueryFromDB(model_name)
-         std::string automata_xml_path = "./resources/xmls/graphAutomata/circle_automata.xml";
+         std::string automata_xml_path = "./resources/xmls/graphAutomata/ellipse_automata.xml";
          if(automata_xml_path.empty())
              return "";
          auto bound_func = std::bind(&GraphFactory::FillUp, this, std::placeholders::_1, std::placeholders::_2);
@@ -187,7 +193,7 @@ std::string RenderCU::BresenhamLine(const SingleAutomata& graph_model)
     return result.dump();
 }
 
-std::string RenderCU::BresenhamCircle(const SingleAutomata& graph_model)
+std::string RenderCU::BresenhamEllipse(const SingleAutomata& graph_model)
 {
     json init_status, terminate_status;
     if (!file_manager.TransStr2JsonObject(graph_model.init_status, init_status))
@@ -207,71 +213,42 @@ std::string RenderCU::BresenhamCircle(const SingleAutomata& graph_model)
     std::vector<std::pair<float, float>> points_part_2;
     std::vector<std::pair<float, float>> points_part_3;
     std::vector<std::pair<float, float>> points_part_4;
-    std::vector<std::pair<float, float>> points_part_5;
-    std::vector<std::pair<float, float>> points_part_6;
-    std::vector<std::pair<float, float>> points_part_7;
-    std::vector<std::pair<float, float>> points_part_8;
 
     // 若不需要动画效果，更改以下points_part_1~points_part_8为points_part即可。
     auto add_circle_points = [&](int xc, int yc, int x, int y) {
         points_part_1.push_back({ xc + x, yc + y });
-        points_part_2.push_back({ xc + y, yc + x });
-        points_part_7.push_back({ xc - y, yc + x });
-        points_part_8.push_back({ xc - x, yc + y });
-        points_part_5.push_back({ xc - x, yc - y });
-        points_part_6.push_back({ xc - y, yc - x });
-        points_part_3.push_back({ xc + y, yc - x });
+        points_part_2.push_back({ xc - x, yc + y });
+        points_part_3.push_back({ xc - x, yc - y });
         points_part_4.push_back({ xc + x, yc - y });
         };
 
     float xc = init_status["x"];
     float yc = init_status["y"];
-    float r = terminate_status["radius"];
+    float a = terminate_status["a"];
+    float b = terminate_status["b"];
 
-    /*int x = 0;
-    int y = r;
-    int d = 3 - 2 * r;
-
-    while (y >= x)
+    float x = 0;
+    float y = b;
+    float dd = 0;
+    float index = 1; // 如果设为0，则初始点会被纳入，会有一个重复的像素
+    while (x <= a && y >=0) // 必须指定x和y的边界，否则可能因为曲线过于陡峭使得控制系数增长极度缓慢时，造成内外while的死循环
     {
-        add_circle_points(xc, yc, x, y);
-        ++x;
-        if (d > 0)
-        {
-            --y;
-            d = d + 4 * (x - y) + 10;
-        }
-        else
-        {
-            d = d + 4 * x + 6;
-        }
-    }*/
-
-    int x = 0;
-    int y = r;
-    int dd = 0;
-    int index = 1;
-    while (x <= y)
-    {
-        dd = 2 * index * r - index * index;
-        while (x * x <= dd)
+        dd = 2 * b * index * a * a - index * index * a * a;
+        while (b * b * x * x <= dd) // 取 <= 会偏向取平行于x轴，取 < 会偏向取平行y轴
         {
             add_circle_points(xc, yc, x, y);
             x++;
         }
+        add_circle_points(xc, yc, x, y - 1); // 当y方向曲线过于陡峭的时候，一个x可能跨过多个下降标志，所以决策每次x跳过一个下降标志，需要直接下降。
         index++;
         y--;
     }
 
     // 若不需要动画效果，删除以下所有insert
-    points_part.insert(points_part.end(), points_part_1.begin(), points_part_1.end());
-    points_part.insert(points_part.end(), points_part_2.rbegin(), points_part_2.rend());
-    points_part.insert(points_part.end(), points_part_3.begin(), points_part_3.end());
     points_part.insert(points_part.end(), points_part_4.rbegin(), points_part_4.rend());
-    points_part.insert(points_part.end(), points_part_5.begin(), points_part_5.end());
-    points_part.insert(points_part.end(), points_part_6.rbegin(), points_part_6.rend());
-    points_part.insert(points_part.end(), points_part_7.begin(), points_part_7.end());
-    points_part.insert(points_part.end(), points_part_8.rbegin(), points_part_8.rend());
+    points_part.insert(points_part.end(), points_part_3.begin(), points_part_3.end());
+    points_part.insert(points_part.end(), points_part_2.rbegin(), points_part_2.rend());
+    points_part.insert(points_part.end(), points_part_1.begin(), points_part_1.end());
 
     json result;
     for (const auto& point : points_part)
@@ -279,8 +256,6 @@ std::string RenderCU::BresenhamCircle(const SingleAutomata& graph_model)
 
     return result.dump();
 }
-
-
 
 std::function<std::string(const SingleAutomata&)> RenderCU::GetFunctor(std::string func_name)
 {
--- headers/RenderKernel.h
+++ headers/RenderKernel.h
@@ -15,11 +15,7 @@
 class RenderCU
 {
 public:
-    RenderCU()
-    {
-        render_functions["BresenhamLine"] = std::bind(&RenderCU::BresenhamLine, this, std::placeholders::_1);
-        render_functions["BresenhamCircle"] = std::bind(&RenderCU::BresenhamCircle, this, std::placeholders::_1);
-    }
+    RenderCU();
     std::function<std::string(const SingleAutomata&)> GetFunctor(std::string func_name);
 protected:
     /// <summary>
@@ -37,12 +33,12 @@ protected:
     /// <returns></returns>
     virtual std::string BresenhamLine(const SingleAutomata& graph_model);
     /// <summary>
-    /// 递推式：di = sqrt(2 * r * i - i * i)，为了效率取di平方，x往右推进，取八分圆第二部分x<=y为大前提，第二限制x * x < di * di
+    /// 递推式：di = sqrt(2 * r * i - i * i) * a / b，为了效率取di平方再乘b的平方，x往右推进，取八分圆第二部分x<=y为大前提，第二限制x * x < di * di
     /// 每次超过第二限制就让y往下一格
     /// </summary>
     /// <param name="graph_model"></param>
     /// <returns></returns>
-    virtual std::string BresenhamCircle(const SingleAutomata& graph_model);
+    virtual std::string BresenhamEllipse(const SingleAutomata& graph_model);
 private:
     std::map<std::string, std::function<std::string(const SingleAutomata&)>> render_functions;
     FileManager file_manager;
